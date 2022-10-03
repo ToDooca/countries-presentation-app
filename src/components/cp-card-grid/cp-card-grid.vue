551d1c69bb214392c05f1791214af8d72593e980
Dusan Stankovic <todooc4@gmail.com>
2022-10-04 01:47:34 +0200

    implemented search by name filter

--- src/components/cp-card-grid/cp-card-grid.vue
+++ src/components/cp-card-grid/cp-card-grid.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="wrapper">
     <div class="filters-section">
-      <cp-search placeholder_label="Search for a country..."/>
+      <div class="search-wrapper">
+        <ion-icon name="search" class="search-icon"/>
+        <input class="input-field"
+               type="text"
+               placeholder="Search for a country..."
+               v-model="searchValue"
+        />
+      </div>
       <cp-dropdown label="Filter by Region" class="dropdown">
-        <p>jen</p>
-        <p>jenasda</p>
-        <p>jenasda</p>
-        <p>jenvcvc</p>
+        <p v-for="region in regions" @click="filterRegion">{{ region }}</p>
       </cp-dropdown>
     </div>
     <div class="card-grid-container" :key="countries">
-      <cp-card v-for="country of countries"
-          :image="country.flags.png"
-          :country_name="country.name.common"
-          :population="country.population"
-          :region="country.region"
-          :capital-list="country.capital"
+      <cp-card v-for="country of filteredCountries"
+               :image="country.flags.png"
+               :country_name="country.name.common"
+               :population="country.population"
+               :region="country.region"
+               :capital-list="country.capital"
+               :key="country.name.common"
       />
     </div>
   </div>
@@ -24,21 +29,33 @@
 <script lang="ts">
 import CpCard from "@/components/cp-card/cp-card.vue";
 import store from "@/store";
-import CpSearch from "@/components/cp-search/cp-search.vue";
 import CpDropdown from "@/components/cp-dropdown/cp-dropdown.vue";
+import {defineComponent} from "vue";
+import {Country} from "@/types/country";
 
-export default {
+export default defineComponent({
   name: "cp-card-grid",
-  components: {CpDropdown, CpSearch, CpCard},
+  components: {CpDropdown, CpCard},
   computed: {
     countries: function () {
       return store.getters.getCountries
+    },
+    filteredCountries: function () {
+      return this.countries.filter((country: Country) =>
+          country.name.common.toLowerCase().includes(this.searchValue.toLowerCase()))
     }
   },
+  data() {
+    return {
+      regions: ['Europe', 'Asia', 'Oceania', 'Americas', 'Africa'],
+      searchValue: ''
+    }
+  },
+  methods: {},
   beforeCreate() {
     store.dispatch("fetchCountries");
   }
-}
+});
 </script>
 
 <style scoped lang="scss">
